ddc85ae47834b4495eeb31bcf0996334aeb1fe21
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-10-26 21:04:42 -0700

    Added missing JS methods with polyfill for IE.

---
+++ js/polyfill/polyfill.js
@@ -0,0 +1,13 @@
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
