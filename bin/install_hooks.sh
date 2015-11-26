7989c61d3914ddd999f253fa75940debf6af2ac9
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-11-26 09:30:16 -0800

    Fixed issue with line ending in pre-commit hook.

--- bin/install_hooks.sh
+++ bin/install_hooks.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 cd .git/hooks/
 ln -s ../../bin/pre-commit pre-commit
--- last_change_date.js
+++ last_change_date.js
@@ -1,1 +1,1 @@
-function getLastChangeDate() { return "Thu, Nov 26, 2015  9:20:48 AM"; }
+function getLastChangeDate() { return "Thu, Nov 26, 2015  9:30:16 AM"; }
