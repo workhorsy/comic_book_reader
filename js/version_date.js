5d0ca1a6f73d555f61547c8531f209db77af91ea
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-06-03 12:57:51 -0700

    Added more output when registering service worker.

--- js/version_date.js
+++ js/version_date.js
@@ -1,1 +1,1 @@
-function getVersionDate() { return "June 03, 2017 - 12:46:52"; }
+function getVersionDate() { return "June 03, 2017 - 12:57:51"; }
--- main.js
+++ main.js
@@ -1053,8 +1053,14 @@ documentOnReady(function() {
 	requireBrowserFeatures(function() {
 		// Start the service worker
 		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register('service-worker.js', { scope: '/' }).then(function() {
-				console.info('Service worker registered ...');
+			navigator.serviceWorker.register('service-worker.js', { scope: '/' }).then(function(reg) {
+				if (reg.installing) {
+					console.log('Service worker installing');
+				} else if (reg.waiting) {
+					console.log('Service worker installed');
+				} else if (reg.active) {
+					console.log('Service worker active');
+				}
 				main();
 			}).catch(function(error) {
 				console.error('Failed to register service worker: ' + error);
